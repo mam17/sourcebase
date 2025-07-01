3b41d3e634cd4e517dc46e53b8a626460c4172f5
Longg <duongthanhlong1710@gmail.com>
2025-07-01 17:20:43 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/data/AppDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.data
+
+//@Database(entities = arrayOf(DetailsSound::class), version = 2, exportSchema = false)
+//abstract class AppDatabase : RoomDatabase() {
+//    companion object {
+//        const val DATABASE_NAME = "ringtone"
+//    }
+//
+//    abstract fun favouriteDao() : FavouriteDao
+//}
